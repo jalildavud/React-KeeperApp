1b5f709730cc6fd10c2f5d48ebede41522706c8a
jalildavud <jdavudlu@gmail.com>
2022-07-04 11:31:56 +0400

    first commmit

---
+++ keeper/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+    
+}
+
+export default App;
