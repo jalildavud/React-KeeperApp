ee18cc863e24765efc5022a3bbdaae9155f25af1
jalildavud <jdavudlu@gmail.com>
2022-07-25 10:33:30 +0400

    second commit

--- keeper/src/App.js
+++ keeper/src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from './notes';
+
+
 
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(noteItem => (
+          <Note 
+          key = {noteItem.key}
+          title = {noteItem.title}
+          content = {noteItem.content}
+          />
+        ))}
       <Footer />
     </div>
   );
